7eeaa97f20c805f97dc6a56fb69b7f37160dc696
Andreas Lind <andreas@one.com>
2016-01-06 14:37:34 +0100

    Initial commit, 1.0.0.

---
+++ README.md
@@ -0,0 +1,79 @@
+node-dnserrors
+==============
+
+[![NPM version](https://badge.fury.io/js/dnserrors.svg)](http://badge.fury.io/js/dnserrors)
+[![Build Status](https://travis-ci.org/One-com/node-dnserrors.svg?branch=master)](https://travis-ci.org/One-com/node-dnserrors)
+
+Exposes a function mapping DNS errors to DnsError objects.
+
+The defined DnsError objects are created via
+<a href="https://github.com/One-com/node-createerror">createerror</a>.
+
+Installation
+------------
+
+Make sure you have node.js and npm installed, then run:
+
+    npm install dnserrors
+
+Usage
+-----
+
+The primary use case is wrapping errors originating from dns operations:
+
+    var dns = require('dns');
+    var dnsErrors = require('dnserrors');
+
+    dns.lookup('qovwiejiqvwiejvoqwevqwev.com', function (err) {
+        var dnsError = dnsErrors(err);
+
+        console.warn(dnsError.toString()); // ENOTFOUND: getaddrinfo ENOTFOUND
+    });
+
+
+Other errors will be marked as not being dns errors:
+
+    var dnsErrors = require('dnserrors');
+
+    var err = new Error();
+    var dnsError = dnsErrors(err);
+
+    if (dnsError.NotDnsError) {
+        // what am I?
+    }
+
+Mappings
+--------
+
+The following is a list of dns errors mapped by this module:
+
+* ENODATA (mapped to 404)
+* EFORMERR (mapped to 400)
+* ESERVFAIL (mapped to 502)
+* ENOTFOUND (mapped to 404)
+* ENOENT (mapped to 404)
+* ENOTIMP (mapped to 501)
+* EREFUSED (mapped to 403)
+* EBADQUERY (mapped to 400)
+* EBADNAME (mapped to 400)
+* EBADFAMILY (mapped to 400)
+* EBADRESP (mapped to 502)
+* ECONNREFUSED (mapped to 502)
+* ETIMEOUT (mapped to 504)
+* EEOF (mapped to 500)
+* EFILE (mapped to 500)
+* ENOMEM (mapped to 500)
+* EDESTRUCTION (mapped to 500)
+* EBADSTR (mapped to 400)
+* EBADFLAGS (mapped to 400)
+* ENONAME (mapped to 400)
+* EBADHINTS (mapped to 400)
+* ENOTINITIALIZED (mapped to 500)
+* ELOADIPHLPAPI (mapped to 500)
+* EADDRGETNETWORKPARAMS (mapped to 500)
+* ECANCELLED (mapped to 500)
+
+License
+-------
+
+3-clause BSD license -- see the `LICENSE` file for details.
---
+++ test/dnsErrors.js
@@ -0,0 +1,98 @@
+var expect = require('unexpected');
+var httpErrors = require('httperrors');
+var dns = require('dns');
+var dnsErrorCodesMap = require('../lib/dnsErrorCodesMap');
+var dnsErrors = require('../lib/dnsErrors');
+
+describe('dnsErrors', function () {
+
+    it('will create a properly subclassed instance', function (done) {
+        // capture a genuine NOTFOUND error
+        dns.lookup('i.really.do.not.exist.vqweuvqwuevqhwieuvhqwkevåjqkrv23jkvewqjvkjqwiouvedjkqwiuvehqwev.com', function (err) {
+            expect(err, 'to be truthy');
+            expect(err.code, 'to equal', 'ENOTFOUND');
+            var dnsError = dnsErrors(err);
+            var httpError = new httpErrors[404]();
+
+            expect(dnsError, 'to equal', new dnsErrors[err.code](err));
+
+            // has the original error propeties
+            expect(dnsError, 'to have properties', Object.keys(err));
+
+            // has the httpError properties
+            expect(dnsError, 'to have properties', Object.keys(httpError));
+
+            // has named errorCode property
+            expect(dnsError[err.code], 'to be true');
+
+            expect(dnsError, 'to be a', dnsErrors.DnsError);
+
+            done();
+        });
+    });
+
+    it('will return unknown error if it was not mapped', function () {
+        var err = new Error();
+        var dnsError = dnsErrors(err);
+
+        expect(dnsError, 'to equal', new dnsErrors.NotDnsError());
+
+        // has named errorCode property
+        expect(dnsError.NotDnsError, 'to be true');
+    });
+
+    it('will not alter the original error', function () {
+        var err = new Error();
+        err.code = 'ECONNREFUSED';
+        var dnsError = dnsErrors(err);
+
+        // assert dnsError was altered
+        expect(dnsError, 'to equal', new dnsErrors[err.code](err));
+
+        // assert orignal err was untouched
+        expect(err, 'not to have properties', ['statusCode']);
+    });
+
+    // check the various error codes will be transformed correctly
+    Object.keys(dnsErrorCodesMap).forEach(function (errorCode) {
+        var statusCode = dnsErrorCodesMap[errorCode];
+
+        describe(errorCode, function () {
+            it('is correctly instantiated', function () {
+                var err = new Error();
+                err.code = errorCode;
+                var dnsError = dnsErrors(err);
+
+                expect(dnsError, 'to equal', new dnsErrors[errorCode](err));
+
+                // has named errorCode property
+                expect(dnsError[errorCode], 'to be true');
+            });
+
+            it('returns a ' + statusCode, function () {
+                var dnsError = dnsErrors((function () {
+                    var err = new Error();
+                    err.code = errorCode;
+                    return err;
+                })());
+
+                expect(dnsError.statusCode, 'to equal', statusCode);
+            });
+
+            it('lets the `code` from the original instance take precedence over the one built into the class', function () {
+                var err = new Error();
+                err.code = 'SOMETHINGELSE';
+                var dnsError = dnsErrors(err);
+
+                expect(dnsError.code, 'to equal', 'SOMETHINGELSE');
+            });
+
+            describe('when instantiated via the constructor', function () {
+                it('has a `code` property', function () {
+                    expect(new dnsErrors[errorCode]().code, 'to equal', errorCode);
+                });
+            });
+        });
+    });
+
+});
